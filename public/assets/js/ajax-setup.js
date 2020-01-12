e04a70079d2ecbebcff6d42e36a05b79d6c1ffc4
Vincent Pang <vpang23@gmail.com>
2020-01-12 06:05:56 +0000

    Initial commit

---
+++ public/assets/js/ajax-setup.js
@@ -0,0 +1,14 @@
+$.ajaxSetup({
+  headers: setHeaders()
+});
+
+function setHeaders() {
+  let jwt = localStorage.getItem('authorization');
+
+  if (jwt) {
+    return {
+      authorization: jwt
+    };
+  }
+  return{};
+}
